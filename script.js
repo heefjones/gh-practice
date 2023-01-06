dccacadcd83f8016b47834afe9d0b9168f490463
Heath <jimmyhj9@gmail.com>
2023-01-06 14:53:30 -0600

    Added script.js and the pdf source file to pull qoutes from. Installed a node package to parse from pdf, and it looks like all the associated files have also been added to the repo. Oops

---
+++ script.js
@@ -0,0 +1,8 @@
+const nouns = ['he', 'her', 'him', 'i', 'man', 'me', 'she', 'woman', 'you'];
+const verbs = ['do', 'does', 'will'];
+const articles = ['a', 'an', 'and', 'after', 'berfore', 'of', 'the', 'to'];
+
+let string = '';
+
+
+
